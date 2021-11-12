c9fa247d044836f457728e1d6ca96dd4c63dde54
Gunda Sreedhar <shreedharguptha2@gmail.com>
2021-11-12 19:57:00 +0530

    added constants class and change in app.java

--- src/main/java/com/gunda/app/git_demo/App.java
+++ src/main/java/com/gunda/app/git_demo/App.java
@@ -1,19 +1,14 @@
 package com.gunda.app.git_demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	public void addData(String data)
-	{
-		System.out.println("this is appData method");
-		if(data.equals("shreedhar"))
+
+public class App {
+	public void addData(String data) {
+		if (data.equals(Contstants.DATA_TYPE))
 			System.out.println("data is correct");
+
 	}
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
---
+++ src/main/java/com/gunda/app/git_demo/Contstants.java
@@ -0,0 +1,5 @@
+package com.gunda.app.git_demo;
+
+public class Contstants {
+	public static String DATA_TYPE = "gunda_sreedhar";
+}
